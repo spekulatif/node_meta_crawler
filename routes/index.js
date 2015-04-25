7f3107791172bd1450f3ee53c74902af610e67b7
spekulatif <ufuktandogan@windowslive.com>
2015-04-25 15:50:00 +0300

    Add parsing meta-data functionality and give some style to pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+   router  = express.Router(),
+   request = require('request'),
+   cheerio = require('cheerio'),
+   URL     = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.render('index', { title: 'Node Meta Crawler' });
 });
+
+router.post('/', function(req, res) {
+ var url = putFormatToUrl(req.body.url);
+
+ request.get({uri: url}, function(error, response, body) {
+   if (!error && response.statusCode === 200) {
+     res.render('result', { metaTags: getMetaTags(body) });
+   } else {
+     res.render('index', { error: 'Sorry! Could not parse your url, Please check your url.' })
+   }
+ });
+});
+
+function putFormatToUrl (url) {
+ return URL.parse(url).protocol ? url : "http://" + url;
+}
+
+function getMetaTags (body) {
+ var parsedBody = cheerio.load(body);
+
+ return {
+   title:        parsedBody('title').text(),
+   description:  parsedBody("meta[name='description']").attr('content'),
+   author:       parsedBody("meta[name='author']").attr('content'),
+   keywords:     parsedBody("meta[name='keywords']").attr('content'),
+   fbImage:      parsedBody("meta[property='og:image']").attr('content')
+ }
+} 
 
 module.exports = router;
